37341bb24822a29c47d06910f49427b8dc9ef237
Payton McIntosh <pmcintosh@df12.net>
2025-11-09 01:09:33 +0000

    Add initial repo skeleton

---
+++ README.md
@@ -0,0 +1,3 @@
+# Common Table Expressions for Diesel
+
+This is a generated project using [Copier](https://copier.readthedocs.io/).
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! Common Table Expressions for Diesel
+
+/// Returns a greeting for the library.
+#[must_use]
+pub fn greet() -> &'static str {
+    "Hello from Common Table Expressions for Diesel!"
+}
